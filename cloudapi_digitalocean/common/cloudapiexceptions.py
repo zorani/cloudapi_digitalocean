76493082b27657b7dcec19d4496b3dd2568997f3
zoran <zorani@gmail.com>
2021-06-11 17:47:21 +0100

    save

--- cloudapi_digitalocean/common/cloudapiexceptions.py
+++ cloudapi_digitalocean/common/cloudapiexceptions.py
@@ -1,3 +1,18 @@
 class ErrorDropletNotFound(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorVolumeAlreadyExists(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorVolumeNotFound(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorSnapshotNotFound(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
