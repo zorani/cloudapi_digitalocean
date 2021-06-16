a03c39fafa8c32e718dd569adb6d1f8d29f5c73e
zoran <zorani@gmail.com>
2021-06-16 13:03:07 +0100

    save

--- cloudapi_digitalocean/common/cloudapiexceptions.py
+++ cloudapi_digitalocean/common/cloudapiexceptions.py
@@ -13,6 +13,56 @@ class ErrorVolumeNotFound(Exception):
         Exception.__init__(self, *args, **kwargs)
 
 
+class ErrorVolumeResizeValueTooLarge(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorVolumeResizeDirection(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
 class ErrorSnapshotNotFound(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorDropletNameContainsSpaces(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorActionDoesNotExists(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorActionFailed(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorDropletSlugSizeNotFound(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorDropletNameContainsInvalidChars(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorDropletResizeDiskError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorDropletAttachedVolumeCountAlreadAtLimit(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ErrorNotSameRegion(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
