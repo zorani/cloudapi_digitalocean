183b5b6d11f9a5fb2865582f8a39b6f2a6824b3c
zoran <zorani@gmail.com>
2021-06-17 14:47:52 +0100

    save

--- cloudapi_digitalocean/__init__.py
+++ cloudapi_digitalocean/__init__.py
@@ -1,7 +1,15 @@
+from .common.cloudapiexceptions import ErrorDropletNotFound
 from .digitaloceanobjects.droplet import Droplet as Droplet
 from .digitaloceanobjects.droplet import DropletManager as DropletManager
 from .digitaloceanobjects.volume import Volume as Volume
 from .digitaloceanobjects.volume import VolumeManager as VolumeManager
 from .digitaloceanobjects.snapshot import Snapshot as Snapshot
 from .digitaloceanobjects.snapshot import SnapshotManager as SnapshotManager
-from .common.cloudapiexceptions import ErrorDropletNotFound
+from .digitaloceanobjects.action import Action as Action
+from .digitaloceanobjects.action import ActionManager as ActionManager
+from .digitaloceanobjects.size import Size as Size
+from .digitaloceanobjects.size import SizeManager as SizeManager
+from .digitaloceanobjects.floatingip import FloatingIP as FloatingIP
+from .digitaloceanobjects.floatingip import FloatingIPManager as FloatingIPManager
+from .digitaloceanobjects.account import Account as Account
+from .digitaloceanobjects.account import AccountManager as AccountManager
